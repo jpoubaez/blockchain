29008db35e6a0879cacda64a08e832a2fac12e1c
jpoubaez <joanpoubaez@gmail.com>
2024-04-04 19:24:03 +0200

    creem multiples nodes en paralel

---
+++ dev/networkNode.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+const uuid = require('uuid/v1');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const nodeAddress = uuid().split('-').join(''); // traiem els - que hi hagin i hi posem espai
+
+const port = process.argv[2]; // rebre per quin port escoltarà
+
+const bitcoin = new Blockchain();
+
+app.get('/blockchain', function(req, res) {
+    res.send(bitcoin);
+});
+
+app.post('/transaction', function(req, res) {
+    const blockIndex = bitcoin.createNewTransaction(req.body.amount,
+    req.body.sender, req.body.recipient) 
+    res.json({ note:`Transaction will be added in block
+    ${blockIndex}.`});
+});
+
+app.get('/mine', function(req, res) {
+
+    const lastBlock = bitcoin.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+
+    bitcoin.createNewTransaction(12.5, "00", nodeAddress); // recompensa, a quina adreça?? la creem
+    
+    const currentBlockData = {
+        transactions: bitcoin.pendingTransactions,
+        index: lastBlock['index'] + 1
+    };
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+    
+
+    res.json({
+        note: "New block mined successfully",
+        block: newBlock
+    });
+
+});
+
+app.listen(port, function(){ // ara escoltem pel port que ens passin
+    console.log(`listening per el port ${port} ...`); 
+
+});
