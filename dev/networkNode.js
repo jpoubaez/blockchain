492000b0720d9c76a25e5e0a9611ba4756527f8a
jpoubaez <joanpoubaez@gmail.com>
2024-04-06 13:36:32 +0200

    endpoint registrar i difondre

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const uuid = require('uuid/v1');
+const rp = require('request-promise');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -46,6 +47,41 @@ app.get('/mine', function(req, res) {
     });
 
 });
+
+app.post('/register-and-broadcast-node', function(req, res) { // afegim el nou node si no el teníem a networkNodes[]
+    const newNodeUrl = req.body.newNodeUrl;
+    if (bitcoin.networkNodes.indexOf(newNodeUrl) == -1)
+        bitcoin.networkNodes.push(newNodeUrl);
+    const regNodesPromises = []; // hi posarem tots els que tenim registrats i necessiten les dades del nou
+    bitcoin.networkNodes.forEach(networkNodeUrl => { // difondre'l cap a tothom excepte el nou
+        // enviar a cada node ja existent una peticio register-node amb les dades del nou
+        bitcoin.networkNodes.forEach(networkNodeUrl => {
+            const requestOptions = { // les dades del request
+                uri: networkNodeUrl + '/register-node', // diferent per a cada destí
+                method: 'POST',
+                body: { newNodeUrl: newNodeUrl }, // tots reben lo mateix: el nou
+                json: true
+            }
+        }
+        regNodesPromises.push(rp(requestOptions)); // preparat per a cada node ja existent
+    }; // end of difondre'l cap a tothom excepte el nou
+
+    Promise.all(regNodesPromises) // enviem a tots
+    .then(data => { // si ha anat bé, li diem al nou la info dels nodes ja existents
+        const bulkRegisterOptions = { 
+            uri: newNodeUrl + '/register-nodes-bulk'  
+            method: 'POST',
+            body: {allNetworkNodes: [...bitcoin.networkNodes,
+            bitcoin.currentNodeUrl]} 
+            json:true
+        }
+
+        return rp(bulkRegisterOptions);
+    }) // si ha anat bé, missatge exit
+    .then(data => {
+        res.json({ note: 'New node registered with network successfully.' });
+    });
+};
 
 app.listen(port, function(){ // ara escoltem pel port que ens passin
     console.log(`listening per el port ${port} ...`); 
